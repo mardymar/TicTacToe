a681d1ce39759f1389ce8db7969f4356be11f65d
mardymar <mpkorea@gmail.com>
2017-09-25 15:32:02 -0700

    Added board

---
+++ TicTacToe.js
@@ -0,0 +1,38 @@
+var turn = 'X';
+var board = [];
+
+for(var i = 0; i < 3; i++) {
+  for(var j = 0; j < 3; j++) {
+    board[i][j] = '';
+  }
+}
+
+$( document ).ready(function() {
+  $("#00").click(function(){
+    $("#00").text("X");
+  });
+  $("#01").click(function(){
+
+  });
+  $("#02").click(function(){
+
+  });
+  $("#10").click(function(){
+
+  });
+  $("#11").click(function(){
+
+  });
+  $("#12").click(function(){
+
+  });
+  $("#20").click(function(){
+
+  });
+  $("#21").click(function(){
+
+  });
+  $("#22").click(function(){
+
+  });
+});
